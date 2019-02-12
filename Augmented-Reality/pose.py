54e99912d6ec3e6c18d1372350e6d53df41b4523
mobiusKey <wallacemark77@gmail.com>
2019-02-12 12:43:03 -0500

    working pose estimation

--- Augmented-Reality/pose.py
+++ Augmented-Reality/pose.py
@@ -1,21 +1,41 @@
-import os
 import cv2
-import cv2.aruco as aruco
 import numpy as np
+import glob
 
-#camera calibration parameters 
-calibrationFile = "calibrationFileName.xml"
-calibrationParams = cv2.FileStorage(calibrationFile, cv2.FILE_STORAGE_READ)
-camera_matrix = calibrationParams.getNode("cameraMatrix").mat()
-dist_coeffs = calibrationParams.getNode("distCoeffs").mat()
+with np.load('pose_webcam_calibration_output.npz') as X:
+	mtx, dist, _, _= [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
 
-r = calibrationParams.getNode("R").mat()
-new_camera_matrix = calibrationParams.getNode("newCameraMatrix").mat()
+def draw(img, corners, imgpts):
+    corner = tuple(corners[0].ravel())
+    img = cv2.line(img, corner, tuple(imgpts[0].ravel()), (255,0,0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[1].ravel()), (0,255,0), 5)
+    img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0,0,255), 5)
+    return img
+	
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
-image_size = (1920, 1080)
-map1, map2 = cv2.fisheye.initUndistortRectifyMap(camera_matrix, dist_coeffs, r, new_camera_matrix, image_size, cv2.CV_16SC2)
+axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
 
-aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_1000)
+for fname in glob.glob('*.jpg'):
+    img = cv2.imread(fname)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
 
-marker_length = 20
-arucoParams = aruco.DetectorParameters_create()
+    if ret == True:
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+
+        # Find the rotation and translation vectors.
+        _, rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners2, mtx, dist)
+
+        # project 3D points to image plane
+        imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+
+        img = draw(img,corners2,imgpts)
+        cv2.imshow('img',img)
+        k = cv2.waitKey(0) & 0xff
+        if k == 's':
+            cv2.imwrite(fname[:6]+'.png', img)
+
+cv2.destroyAllWindows()
