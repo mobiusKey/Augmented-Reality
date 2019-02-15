cce90cfee9056e6e65e7579dcb87bfa8a00df768
mobiusKey <wallacemark77@gmail.com>
2019-02-14 21:00:46 -0500

    android streaming now does aruco pose estimation, added webcam class for easier use. Needs some cleanup. android ids valueerror: truth of an array with more than one element is ambiguous

--- Augmented-Reality/android_stream.py
+++ Augmented-Reality/android_stream.py
@@ -1,22 +1,42 @@
-import requests
 import numpy as np
 import cv2
+from cv2 import aruco
+from webcam import Webcam
 
-print("starting")
+
+print("starting...")
+
+webcam = Webcam()
+webcam.start()
+with np.load('pose_webcam_calibration_output.npz') as X:
+	mtx, dist, _, _= [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
+	
+image_size = (1920, 1080)
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_1000)
+
+markerLength = 1
+
+arucoParams = aruco.DetectorParameters_create()
 
 
-
-stream = cv2.VideoCapture('http://IPAddress/video')
+#stream = cv2.VideoCapture('http://104.38.59.31:8080/video')
 
 # Use the next line if your camera has a username and password
 # stream = cv2.VideoCapture('protocol://username:password@IP:port/1')  
 
 while True:
+	img = webcam.get_current_frame()
+	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=arucoParams)
+	if ids != None:
+		rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corners, markerLength, mtx, dist)
+		imgAruco = aruco.drawDetectedMarkers(img, corners, ids, (0,255,0))
+		imgAruco = aruco.drawAxis(imgAruco, mtx, dist, rvec, tvec, 1)
+	else:
+		print("marker not detected")
+		imgAruco = img
+	cv2.imshow("aruco", img)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		cv2.destroyAllWindows()
+		break
 
-    r, f = stream.read()
-    cv2.imshow('IP Camera stream',f)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cv2.destroyAllWindows()
