4d2179e5847b8e2a1612d2cadd8ee99c31d00aba
mobiusKey <wallacemark77@gmail.com>
2019-02-19 20:39:40 -0500

    full control of model position

--- Augmented-Reality/objviewer.py
+++ Augmented-Reality/objviewer.py
@@ -25,7 +25,7 @@ glEnable(GL_LIGHT0)
 glEnable(GL_LIGHTING)
 glEnable(GL_COLOR_MATERIAL)
 glEnable(GL_DEPTH_TEST)
-glShadeModel(GL_SMOOTH)           # most obj files expect to be smooth-shaded
+#glShadeModel(GL_SMOOTH)		   # most obj files expect to be smooth-shaded
 
 # LOAD OBJECT AFTER PYGAME INIT
 obj = OBJ('./models/Dragon', swapyz=True)
@@ -41,39 +41,83 @@ glMatrixMode(GL_MODELVIEW)
 
 rx, ry = (0,0)
 tx, ty = (0,0)
+zrot = 0
 zpos = 5
 rotate = move = False
-while 1:
-    clock.tick(30)
-    for e in pygame.event.get():
-        if e.type == QUIT:
-            sys.exit()
-        elif e.type == KEYDOWN and e.key == K_ESCAPE:
-            sys.exit()
-        elif e.type == MOUSEBUTTONDOWN:
-            if e.button == 4: zpos = max(1, zpos-1)
-            elif e.button == 5: zpos += 1
-            elif e.button == 1: rotate = True
-            elif e.button == 3: move = True
-        elif e.type == MOUSEBUTTONUP:
-            if e.button == 1: rotate = False
-            elif e.button == 3: move = False
-        elif e.type == MOUSEMOTION:
-            i, j = e.rel
-            if rotate:
-                rx += i
-                ry += j
-            if move:
-                tx += i
-                ty -= j
+while True:
+	keys = pygame.key.get_pressed()
+	
+	for e in pygame.event.get():
+		if e.type == QUIT:
+			sys.exit()
+		elif e.type == KEYDOWN and e.key == K_ESCAPE:
+			sys.exit()
+		elif e.type == MOUSEBUTTONDOWN:
+			if e.button == 4: zpos = max(1, zpos-1)
+			elif e.button == 5: zpos += 1
+			elif e.button == 1: rotate = True
+			elif e.button == 3: move = True
+		elif e.type == MOUSEBUTTONUP:
+			if e.button == 1: rotate = False
+			elif e.button == 3: move = False 
+		elif e.type == MOUSEMOTION:
+			i, j = e.rel
+			if rotate:
+				rx += i
+				ry += j
+			if move:
+				tx += i
+				ty -= j
+			
+			
+	if keys[pygame.K_w]:
+		ty += 1
+		print("w was pressed")
+	if keys[pygame.K_a]:
+		tx -= 1
+		print("a was pressed")			
+	if keys[pygame.K_s]:
+		ty -= 1
+		print("s was pressed")
+	if keys[pygame.K_d]:
+		tx += 1
+		print("d was pressed")
+		
+	if keys[pygame.K_UP]:
+		ry += 1
+	if keys[pygame.K_DOWN]:
+		ry -= 1
+	if keys[pygame.K_LEFT]:
+		rx -= 1
+	if keys[pygame.K_RIGHT]:
+		rx += 1
+		
+	if keys[pygame.K_SPACE]:
+		zpos += 1
+	if keys[pygame.K_LSHIFT]:
+		zpos -= 1
+	if keys[pygame.K_PERIOD]:
+		zrot += 1
+	if keys[pygame.K_COMMA]:
+		zrot -= 1
+		
+	print("x:{} y:{} z:{}\nrx:{} ry:{}".format(tx, ty, zpos, rx, ry))
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glLoadIdentity()
+	clock.tick(30)
 
-    # RENDER OBJECT
-    glTranslate(tx/20., ty/20., - zpos)
-    glRotate(ry, 1, 0, 0)
-    glRotate(rx, 0, 1, 0)
-    glCallList(obj.gl_list)
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+	glLoadIdentity()
 
-    pygame.display.flip()
+	# RENDER OBJECT
+	#scaling seems to help with the dragon
+	glScalef(0.5,0.5,0.5)
+	
+	glTranslate(tx/20., ty/20., - zpos)
+	glRotate(ry, 1, 0, 0)
+	glRotate(rx, 0, 1, 0)
+	# need to add yaw gl rotate
+	glRotate(zrot, 0, 0, 1)
+	glCallList(obj.gl_list)
+	
+
+	pygame.display.flip()
--- Augmented-Reality/webcam.py
+++ Augmented-Reality/webcam.py
@@ -3,21 +3,21 @@ from threading import Thread
   
 class Webcam:
   
-    def __init__(self):
-        self.video_capture = cv2.VideoCapture('http://104.38.59.31:8080/video')
-        self.current_frame = self.video_capture.read()[1]
-        self.run = True
-          
-    # create thread for capturing images
-    def start(self):
-        Thread(target=self._update_frame, args=()).start()
+	def __init__(self):
+		self.video_capture = cv2.VideoCapture('http://104.38.59.31:8080/video')
+		self.current_frame = self.video_capture.read()[1]
+		self.run = True
+		  
+	# create thread for capturing images
+	def start(self):
+		Thread(target=self._update_frame, args=()).start()
   
-    def _update_frame(self):
-        while(self.run):
-            self.current_frame = self.video_capture.read()[1]
-                  
-    # get the current frame
-    def get_current_frame(self):
-        return self.current_frame
-    def stop(self):
-        self.run = False
+	def _update_frame(self):
+		while(self.run):
+			self.current_frame = self.video_capture.read()[1]
+				  
+	# get the current frame
+	def get_current_frame(self):
+		return self.current_frame
+	def stop(self):
+		self.run = False
